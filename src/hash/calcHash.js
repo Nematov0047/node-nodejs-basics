251f51169ea2b29be2be9d95a514cc7f4d9dfafc
G'anisher <cyberredweb@gmail.com>
2024-10-06 14:35:50 +0500

    modified:   src/hash/calcHash.js 	modified:   src/streams/files/fileToWrite.txt 	modified:   src/streams/read.js 	modified:   src/streams/write.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -6,6 +6,8 @@ const calculateHash = async () => {
     let hex_crypt = crypto.createHash('sha256').update('salom', 'utf-8').digest('hex');
     console.log(hex_crypt);
 
+    // I will do streams api later
+
 };
 
 await calculateHash();
--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,11 @@
+import { createReadStream } from 'node:fs';
+
 const read = async () => {
     // Write your code here 
+    const stream = createReadStream('./files/fileToRead.txt');
+
+    stream.pipe(process.stdout);
+
 };
 
 await read();
