f10ed65a7c02a727e6ec610b2632cff0ced50130
G'anisher <cyberredweb@gmail.com>
2024-10-07 23:31:42 +0500

    modified:   src/cp/cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,24 @@
+import { spawn } from 'node:child_process';
+
 const spawnChildProcess = async (args) => {
     // Write your code here
+    const child_process = spawn('node', ['./files/script.js', ...args]);
+
+    process.stdin.pipe(child_process.stdin);
+    child_process.stdout.pipe(process.stdout);
+
+    // child_process.stdout.on('data', (data) => {
+    //     console.log(`stdout: ${data}`);
+    // });
+
+    child_process.stderr.on('data', (data) => {
+        console.log(`stderr: , ${data}`);
+    });
+
+    child_process.on('close', (code) => {
+        console.log(`child process exited with code , ${code}`);
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['test1', 'test2'] /* [someArgument1, someArgument2, ...] */);
