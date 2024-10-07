383320814d0605931d2b16ad2ccd4eff64b64a4b
G'anisher <cyberredweb@gmail.com>
2024-10-07 22:29:49 +0500

    new file:   src/fs/files/fileToRemove.txt 	renamed:    src/fs/files/properFilename.md -> src/fs/files/wrongFilename.txt 	modified:   src/wt/main.js 	modified:   src/wt/worker.js 	deleted:    src/zip/files/archive.gz 	deleted:    src/zip/files/fileToCompress2.txt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+
 const performCalculations = async () => {
     // Write your code here
+    const number_of_logical_cores = os.cpus().length;
+
+    const newWorker = (n) => {
+        return new Promise((resolve, reject) => {
+            const worker = new Worker('./worker.js', { workerData: { n: n } });
+
+            worker.on('message', (result) => {
+                resolve({
+                    status: 'resolved',
+                    data: result
+                });
+            });
+
+            worker.on('error', () => {
+                reject({
+                    status: 'error',
+                    data: null
+                });
+            })
+        });
+    }
+
+    const workerPromises = [];
+    for (let i = 0; i < number_of_logical_cores; i++) {
+        workerPromises.push(newWorker(10 + i));
+    }
+
+    const final_results = await Promise.allSettled(workerPromises);
+    console.log(final_results.map((item) => item.value));
+
 };
 
 await performCalculations();
